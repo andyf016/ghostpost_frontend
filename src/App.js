6e0d0b769014a503fb9770ee013855bc5bdae6a2
Andrew Fillenwarth <andyf016@gmail.com>
2020-09-30 23:17:25 -0400

    setting up boilerplate code

--- src/App.js
+++ src/App.js
@@ -2,25 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      posts: []
+    }
+  }
+  render(){
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+     {this.state.posts.map(p => (
+       <li>{p.body}</li>
+       // TODO: add more post info
+     ))}
+     </ul>
     </div>
   );
+  }
+  
 }
 
 export default App;
